6580961273dadaeafc958d2d2ced5d3b7b3d04d7
Joshua Hunsche Jones <joshua@hunschejones.com>
2018-07-28 22:44:59 -0700

    Add ex4-6

---
+++ ex05.rb
@@ -0,0 +1,15 @@
+name = 'Joshua'
+age = 27
+height = 74
+weight = 195
+eyes = 'Blue'
+hair = 'Blonde'
+
+puts "Let's talk about #{name}."
+puts "He's #{height} inches tall. Thats #{height / 12} feet, #{height % 12} inches."
+puts "He's #{weight} punds heavy."
+puts "Actually his wife says that's not too heavy."
+puts "He's got #{eyes} eyes and #{hair} hair."
+
+# this line is tricky, try to get it exactly right
+puts "If I add #{age}, #{height}, and #{weight} I get #{age + height + weight}."
