c9ee185ac0e7c65cfde2fa8d76a1b834464f085c
Joshua Hunsche Jones <joshua@hunschejones.com>
2018-11-22 12:33:08 -0800

    hello_app initial commit

---
+++ hello_app/README.md
@@ -0,0 +1,3 @@
+# hello_app
+
+This very simple app used the `rails new` scaffold to generate a new web application, which was then modified to serve an html string to the client at the root URL.
---
+++ hello_app/config/routes.rb
@@ -0,0 +1,5 @@
+Rails.application.routes.draw do
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # route patter is 'controller_name#action_name'
+  root 'application#hello'
+end
