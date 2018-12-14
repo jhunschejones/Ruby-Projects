76ae41bca37646fabc73084308902c99bf955bc6
Joshua Hunsche Jones <joshua@hunschejones.com>
2018-12-14 10:09:31 -0800

    Add to users controller, progress on user page and add-user form

--- Microblog/app/helpers/users_helper.rb
+++ Microblog/app/helpers/users_helper.rb
@@ -1,2 +1,9 @@
 module UsersHelper
+
+  # returns the gravitar for a given user
+  def gravitar_for(user)
+    gravitar_id = Digest::MD5::hexdigest(user.email.downcase)
+    gravitar_url = "https://secure.gravatar.com/avatar/#{gravitar_id}"
+    image_tag(gravitar_url, alt: user.name, class: "gravitar")
+  end
 end
