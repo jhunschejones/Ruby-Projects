c45347096ef2edf4f6e9dbc4b7354d6e87ac2bf4
Joshua Hunsche Jones <joshua@hunschejones.com>
2019-05-17 09:05:21 -0700

    Add resourceful routes for `subjects`

--- rails_cms/config/routes.rb
+++ rails_cms/config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
-  get 'subjects/show'
-  get 'subjects/index'
-  get 'subjects/new'
-  get 'subjects/edit'
-  get 'subjects/delete'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # get 'subjects/show'
+  # get 'subjects/index'
+  # get 'subjects/new'
+  # get 'subjects/edit'
+  # get 'subjects/delete'
+  resources :subjects do
+    member do
+      get :delete
+    end
+  end
 end
